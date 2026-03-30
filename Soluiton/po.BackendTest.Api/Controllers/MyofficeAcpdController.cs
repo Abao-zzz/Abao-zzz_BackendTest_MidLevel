b16f849b9f6310045e6b0e332fc9e54ae9a5eb2b
Abao <benson871010@gmail.com>
2026-03-30 18:07:56 +0800

    feat: initialize .NET 8 Web API project with Swagger and base controller setup

---
+++ Soluiton/po.BackendTest.Api/Controllers/MyofficeAcpdController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace po.BackendTest.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MyofficeAcpdController : Controller
+    {
+
+    
+            [HttpGet]
+            public IActionResult GetAll()
+            {
+                return Ok("API is working!");
+            }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
